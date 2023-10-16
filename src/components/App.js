1eea7bc1aaeeba762dc092c8579d8f3735dd6a6f
Mani Poorna <ndmprao2001@gmail.com>
2023-10-16 20:25:08 +0530

    V1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const male = ['Shirts', 'Facial - grooming kits', 'Hair styling', 'Jeans', 'Shoes']
+  const female = ['Tops', 'Skirts', 'Makeup items', 'Jewellery', 'Sandals']
+  const [data, setData] = useState(male)
+  function changeData(e) {
+    if (e === "male") {
+      setData(male)
+    }
+    else {
+      setData(female)
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <select onChange={(e)=>changeData(e.target.value)}>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      <ul >
+      {
+        data && data.map((item) => (
+          <li>{item }</li>
+        ))
+        }
+      </ul>
     </div>
   )
 }
